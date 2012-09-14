cfeea7b228e57b3423f704e3b6af2651bc989e39
98405067@stud.sju.edu.tw <98405067@stud.sju.edu.tw@0ad51b09-5a60-bbc7-012f-b9a670e73318>
2012-09-14 04:05:31 +0000

    HealthApplet add MerchantExp

---
+++ trunk/HealthApplet/src/edu/sju/ee98/health/applet/merchant/FingerPrint.java
@@ -0,0 +1,73 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.sju.ee98.health.applet.merchant;
+
+import edu.sju.ee98.fingerprint.FingerCharacterize;
+import edu.sju.ee98.health.applet.device.FingerModule;
+import edu.sju.ee98.health.applet.device.SerialFinger;
+import edu.sju.ee98.health.applet.panel.CharacterizePanel;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import jssc.SerialPort;
+import jssc.SerialPortException;
+
+/**
+ *
+ * @author Leo
+ */
+public class FingerPrint {
+
+    private FingerModule module;
+
+    public FingerPrint() {
+        module = SerialFinger.scan();
+        if (this.module == null) {
+            JOptionPane.showMessageDialog(null, "Can not connect to the Fingerprint Module!", "Serial Port Error", JOptionPane.ERROR_MESSAGE, null);
+        }
+    }
+
+    public FingerCharacterize gerCharacterize() {
+        if (this.module == null) {
+            JOptionPane.showMessageDialog(null, "Can not connect to the Fingerprint Module!", "Serial Port Error", JOptionPane.ERROR_MESSAGE, null);
+            return null;
+        }
+        FingerCharacterize characterize = null;
+        try {
+            Thread option = new Thread() {
+                @Override
+                public void run() {
+                    JOptionPane.showOptionDialog(null, "Please scan Fingerprint, or click Cancel", "Information", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new String[]{"Cancel"}, null);
+                    module.setTimeout(1);
+                }
+            };
+            option.start();
+            module.setTimeout(0);
+            module.getSerial().openPort();
+            module.getSerial().setParams(SerialPort.BAUDRATE_19200, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+            try {
+                characterize = this.module.getCharacterize();
+            } catch (IOException ex) {
+            }
+            option.interrupt();
+            try {
+                option.join();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(CharacterizePanel.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            module.setTimeout(0);
+            try {
+                module.deleteAll();
+            } catch (IOException ex) {
+                Logger.getLogger(CharacterizePanel.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            module.getSerial().closePort();
+        } catch (SerialPortException ex) {
+            Logger.getLogger(CharacterizePanel.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return characterize;
+    }
+}
